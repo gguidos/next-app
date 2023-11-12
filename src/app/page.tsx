b4b1b44cfa42be731edd1f5bc868e1f405500bf8
Guido Guidos <guido.guidos@gmail.com>
2023-11-12 20:15:42 +0100

    chore(system): initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
 		'mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'
 
 	return (
-		<>
-			<div className={subMainClasses}>
+		<div className={ mainClasses }>
+			<div className={subMainClasses }>
 				<p className="text-sm font-semibold text-gray-700">
 					Quill is now public!
 				</p>
@@ -153,6 +153,6 @@ export default function Home() {
 					</div>
 				</div>
 			</div>
-		</>
+		</div>
 	)
 }
