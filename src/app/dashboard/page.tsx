f472e1e0b527f876ebc688bc852b844208141284
Guido Guidos <guido.guidos@gmail.com>
2023-11-13 08:18:56 +0100

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,4 @@
-import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 
-export default () => {
-    const cookieStore = cookies();
-    const token = cookieStore.get('token');
-
-    if (!token) redirect('/signin');
-
-    return (<h1>Hello</h1>)
-}
+export default () => return (<h1>Hello</h1>)
